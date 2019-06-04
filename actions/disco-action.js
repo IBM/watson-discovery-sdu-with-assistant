1b94abdacc8d366090863724db7bbeab1d115801
Rich Hagarty <rich.hagarty@ibm.com>
2019-06-04 16:32:19 -0700

    initial code (#1)
    * initial code

---
+++ actions/disco-action.js
@@ -0,0 +1,74 @@
+// function main(args) {
+//   console.log('Begin action');
+//   // console.log(args);
+//   //  return new Promise(function(resolve, reject) {
+//   return { text: 'Disco Action Called!' };
+// //  });
+// }
+
+/**
+  *
+  * @param {object} params
+  * @param {string} params.iam_apikey
+  * @param {string} params.url
+  * @param {string} params.username
+  * @param {string} params.password
+  * @param {string} params.environment_id
+  * @param {string} params.collection_id
+  * @param {string} params.configuration_id
+  * @param {string} params.input
+  *
+  * @return {object}
+  *
+  */
+
+const assert = require('assert');
+const DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
+
+/**
+  *
+  * main() will be run when you invoke this action
+  *
+  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+  *
+  * @return The output of this action, which must be a JSON object.
+  *
+  */
+function main(params) {
+  return new Promise(function (resolve, reject) {
+
+    let discovery;
+
+    if (params.iam_apikey){
+      discovery = new DiscoveryV1({
+        'iam_apikey': params.iam_apikey,
+        'url': params.url,
+        'version': '2019-03-25'
+      });
+    }
+    else {
+      discovery = new DiscoveryV1({
+        'username': params.username,
+        'password': params.password,
+        'url': params.url,
+        'version': '2019-03-25'
+      });
+    }
+
+    discovery.query({
+      'environment_id': params.environment_id,
+      'collection_id': params.collection_id,
+      // 'configuration_id': params.configuration_id,
+      'natural_language_query': params.input,
+      'passages': true,
+      'count': 3,
+      'passages_count': 3
+      // 'return': 'text'
+    }, function(err, data) {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(data);
+    });
+  });
+}
