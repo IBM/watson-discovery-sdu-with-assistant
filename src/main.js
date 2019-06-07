d11048c618123d301bd98b6706b55eeb4e2651a1
Rich Hagarty <rich.hagarty@ibm.com>
2019-06-07 12:34:06 -0700

    remove disco references

--- src/main.js
+++ src/main.js
@@ -17,135 +17,46 @@
 import 'isomorphic-fetch';
 import React from 'react';
 import PropTypes from 'prop-types';
-import queryString from 'query-string';
-import Matches from './Matches';
 import Messages from './Messages';
-import { Grid, Header, Card, Input } from 'semantic-ui-react';
+import { Grid, Card, Input } from 'semantic-ui-react';
 
 const utils = require('../lib/utils');
 const util = require('util');  
 
+var messageCounter = 1;
+
 /**
  * Main React object that contains all objects on the web page.
  * This object manages all interaction between child objects as
- * well as making search requests to the discovery service.
+ * well as posting messages to the Watson Assistant service.
  */
 class Main extends React.Component {
   constructor(...props) {
     super(...props);
     const { 
-      // query data
-      data,
-      numMatches,
       error,
-      // query params
-      searchQuery,
-      // assistant data
-      // context,
-      // userInput,
-      // conversation
     } = this.props;
 
     // change in state fires re-render of components
     this.state = {
-      // query data
-      data: data,   // data should already be formatted
-      numMatches: numMatches || 0,
-      loading: false,
       error: error,
-      // query params
-      searchQuery: searchQuery || '',
       // assistant data
       context: {},
       userInput: '',
-      conversation: []
+      conversation: [
+        { id: 1,
+          text: 'Welcome to the Ecobee support team chatbot!',
+          owner: 'watson'
+        }]
     };
-  }
-
-  /**
-   * fetchData - build the query that will be passed to the 
-   * discovery service.
-   */
-  fetchData(query) {
-    const { conversation } = this.state;
-    const searchQuery = query;
-
-    // console.log("QUERY2 - selectedCategories: ");
-    // for (let item of selectedCategories)
-    //   console.log(util.inspect(item, false, null));
-    // console.log("QUERY2 - searchQuery: " + searchQuery);
-    
-    this.setState({
-      loading: true,
-      searchQuery
-    });
-
-    scrollToMain();
-    history.pushState({}, {}, `/${searchQuery.replace(/ /g, '+')}`);
-
-    // build query string, with filters and optional params
-    const qs = queryString.stringify({
-      query: searchQuery,
-      count: 4
-    });
-
-    // send request
-    fetch(`/api/search?${qs}`)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw response;
-        }
-      })
-      .then(json => {
-        var passages = json.passages;
-        passages = utils.formatData(passages);
-        
-        console.log('+++ DISCO RESULTS +++');
-        const util = require('util');
-        console.log(util.inspect(passages, false, null));
-        console.log('numMatches: ' + passages.results.length);
-
-        // add to message list
-        passages.results.forEach(function(result) {
-          conversation.push(
-            { id: conversation.length,
-              text: result.text,
-              owner: 'watson'});
-        });
-
-        this.setState({
-          data: passages,
-          conversation: conversation,
-          loading: false,
-          numMatches: passages.length,
-          error: null
-        });
-        scrollToMain();
-      })
-      .catch(response => {
-        this.setState({
-          error: (response.status === 429) ? 'Number of free queries per month exceeded' : 'Error fetching results',
-          loading: false,
-          data: null
-        });
-        // eslint-disable-next-line no-console
-        console.error(response);
-      });
   }
   
   /**
-   * fetchData - build the query that will be passed to the 
-   * discovery service.
+   * sendMessage - build the message that will be passed to the 
+   * Assistant service.
    */
   sendMessage(text) {
     var { context, conversation } = this.state;
-
-    // context.my_creds = {
-    //   'user':'7a4d1a77-2429-43b1-b6ed-a2b438e15bea',
-    //   'password':'RVVEdpPFLAuuTwFXjjKujPKY0hUOEzt6nQ6O7NwyonHeF7OdAm77Uc34GL2wQHDx'
-    // };
 
     this.setState({
       context: context
@@ -181,29 +92,27 @@ class Main extends React.Component {
           var passages = json.context.webhook_result_1.passages;
           passages = utils.formatData(passages);
           
-          // console.log('+++ DISCO RESULTS +++');
-          // const util = require('util');
-          // console.log(util.inspect(passages, false, null));
-          // console.log('numMatches: ' + passages.results.length);
-
           // add a header to our message
+          messageCounter += 1;
           conversation.push(
-            { id: Math.random(),
+            { id: messageCounter,
               text: 'Here are some excerpts from the Users Guide:',
               owner: 'watson'});
 
           // add to message list
           passages.results.forEach(function(result) {
+            messageCounter += 1;
             conversation.push(
-              { id: Math.random(),
+              { id: messageCounter,
                 text: result.text,
                 owner: 'watson-cont'});
           });
 
         } else {
           // add to message list
+          messageCounter += 1;
           conversation.push(
-            { id: Math.random(),
+            { id: messageCounter,
               text: json.output.text[0],
               owner: 'watson'
             });
@@ -227,35 +136,40 @@ class Main extends React.Component {
       });
   }
 
-  /* Log Watson Assistant context values, so we can follow along with its logic. */
+  /**
+   * Log Watson Assistant context values, so we can follow along with its logic. 
+   */
   printContext(context) {
     if (context.system) {
       if (context.system.dialog_stack) {
-        // console.log(
-        //   '     dialog_stack: [' + util.inspect(context.system.dialog_stack, false, null) + ']');
         console.log('Dialog Stack:');
         console.log(util.inspect(context, false, null));
       }
     }
   }
   
+  /**
+   * Display each key stroke in the UI. 
+   */
   handleOnChange(event) {
     this.setState({userInput: event.target.value});
   }
 
+  /**
+   * Send user message to Assistant. 
+   */
   handleKeyPress(event) {
     const { userInput, conversation } = this.state;
 
     if (event.key === 'Enter') {
+      messageCounter += 1;
       conversation.push(
-        { id: conversation.length,
+        { id: messageCounter,
           text: userInput,
           owner: 'user'
         }
       );
 
-      // console.log('searchQuery [FROM SEARCH]: ' + searchValue);
-      // this.fetchData(searchValue);
       this.sendMessage(userInput);
 
       this.setState({
@@ -268,26 +182,8 @@ class Main extends React.Component {
   }
 
   /**
-   * getMatches - return collection matches to be rendered.
+   * Get list of conversation message to display. 
    */
-  getMatches() {
-    const { data } = this.state;
-
-    if (!data || data.results.length == 0) {
-      return (
-        <Header as='h3' textAlign='center'>
-            No results found. Please enter new search query.
-        </Header>
-      );
-    } else {
-      return (
-        <Matches 
-          matches={ data.results }
-        />
-      );
-    }
-  }
-
   getListItems() {
     const { conversation } = this.state;
 
@@ -299,7 +195,7 @@ class Main extends React.Component {
   }
 
   /**
-   * render - return all the home page object to be rendered.
+   * render - return all the home page objects to be rendered.
    */
   render() {
     const { userInput } = this.state;
@@ -335,9 +231,6 @@ class Main extends React.Component {
   }
 }
 
-//                 value={this.state.userInput}
-//                onKeyPress={this.handleKeyPress.bind(this)}
-
 /**
  * scrollToMain - scroll window to show 'main' rendered object.
  */
@@ -350,9 +243,6 @@ function scrollToMain() {
 
 // type check to ensure we are called correctly
 Main.propTypes = {
-  data: PropTypes.object,
-  searchQuery: PropTypes.string,
-  numMatches: PropTypes.number,
   context: PropTypes.object,
   userInput: PropTypes.string,
   conversation: PropTypes.array,
